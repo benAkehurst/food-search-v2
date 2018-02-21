c2010c525a4fe40d009a7fe81a738d4c8640b9f0
benAkehurst <benakehurst@hotmail.com>
2018-02-21 23:54:17 +0200

    login component defined

---
+++ food-search/src/app/Components/LoginComponent/login.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../Services/data.service';
+import { Http } from '@angular/http/src/http';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { FormGroup, AbstractControl, FormBuilder, Validators } from '@angular/forms';
+import { NgModel } from '@angular/forms';
+
+@Component({
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+    constructor(private dataService: DataService, private router: Router) { }
+    places: any;
+
+    ngOnInit() {
+
+    }
+
+    public goToRegister() {
+        this.router.navigate(['/register']);
+    }
+
+}
